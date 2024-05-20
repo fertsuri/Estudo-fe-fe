61983f932f2809845d0f4fec2e8dbed61df6742f
fertsuri <fsab95@gmail.com>
2024-05-20 13:03:57 -0300

    nova pasta

---
+++ estudo js/teste.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')();
+
+
+const fefe = prompt("Digite sim ou nao para saber se a fe fe é bocó: ");
+
+if (fefe === "sim") {
+    console.log("é bocó")
+}
+else {
+    console.log("não tem jeito, é bocó")
+}
+
+dqwqewqdasxczxczcsdasdasda
+
